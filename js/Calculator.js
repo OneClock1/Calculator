f9ca7d6f10e24008e9921e649cdbf4ed2561ffa7
OneClock1 <toly1539@gmail.com>
2023-02-17 19:13:08 +0200

    Update calculator.js

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,11 +1,12 @@
 
 
 
+
 let getSum = () => sum(document.getElementById('x').value, document.getElementById('y').value);
 let getSubtract = () => subtract(document.getElementById('x').value, document.getElementById('y').value);
 
 let sum = (x, y) =>
-{
+{  
     let num1, num2;
     num1 = parseInt(x);
     num2 = parseInt(y);
