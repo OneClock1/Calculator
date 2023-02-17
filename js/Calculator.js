cdac2f822092c5014e1625abb2b0bef03564a86e
OneClock1 <toly1539@gmail.com>
2023-02-17 18:58:31 +0200

    initial

---
+++ js/Calculator.js
@@ -0,0 +1,23 @@
+
+
+
+let getSum = () => sum(document.getElementById('x').value, document.getElementById('y').value);
+let getSubtract = () => subtract(document.getElementById('x').value, document.getElementById('y').value);
+
+let sum = (x, y) =>
+{
+    let num1, num2;
+    num1 = parseInt(x);
+    num2 = parseInt(y);
+    return num1 + num2; 
+    
+}
+
+let subtract = (x, y) =>
+{
+    let num1, num2;
+    num1 = parseInt(x);
+    num2 = parseInt(y);
+    return num1 - num2; 
+}
+
